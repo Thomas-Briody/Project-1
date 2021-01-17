0fae88976af5e3f006cacaf93589d462abb31e77
Tom <tom@briody.net>
2021-01-17 13:06:11 +0000

    bomb bug fixed, laser bug fixed, collision detection for spaceship

--- app.js
+++ app.js
@@ -1,40 +1,65 @@
 const grid = document.querySelector('.grid')
+const startButton = document.querySelector('button')
 const width = 10
 const cells = []
 let spaceship = 93
-let laser = 0
+const laserTimers = {}
+const bombTimers = {}
+let bombPosition 
 const aliensStart = [2, 4, 6, 7, 13, 15, 16]
 let currentAlienPositions = [2, 4, 6, 7, 13, 15, 16]
 let aliensMovingInterval 
-let laserTimer 
+const pointsTotal = document.querySelector('.points-total')
+const dangerZone = [
+  { letter: 'd', index: 80 },
+  { letter: 'a', index: 81 },
+  { letter: 'n', index: 82 },
+  { letter: 'g', index: 83 },
+  { letter: 'e', index: 84 },
+  { letter: 'r', index: 85 },
+  { letter: 'z', index: 86 },
+  { letter: 'o', index: 87 },
+  { letter: 'n', index: 88 },
+  { letter: 'e', index: 89 }
+]
+let points = 0
+
+function startGame() {
+  //* Call the function to create the grid:
+  createGrid()
+  //* Call the function to add the spaceship
+  addSpaceship()
+  //* Call the function to load the dangerZone:
+  loadDangerZone()
+  //* Call the function to add aliens to the grid:
+  addAliens()
+  //* Call the function to generate a bomb
+  setInterval(() => bombAppear(), 2000)
+}
+
+startButton.addEventListener('click',() => {
+  startGame()
+  startButton.disabled = true
+})
 
 //* Creating the grid: 
 
-for (let index = 0; index < width ** 2; index++) {
+function createGrid() {
+  for (let index = 0; index < width ** 2; index++) {
   // ? Generate each element
-  const cell = document.createElement('div')
-  cell.classList.add('cell')
-  grid.appendChild(cell)
-  cells.push(cell)
-  // ? Number each cell by its index.
-  cell.innerHTML = index
-  // ? Set the width and height of my cells
-  cell.style.width = `${100 / width}%`
-  cell.style.height = `${100 / width}%`
+    const cell = document.createElement('div')
+    cell.classList.add('cell')
+    grid.appendChild(cell)
+    cells.push(cell)
+    // ? Number each cell by its index.
+    cell.innerHTML = index
+    // ? Set the width and height of my cells
+    cell.style.width = `${100 / width}%`
+    cell.style.height = `${100 / width}%`
+  }
 }
 
-//* Calling the function to add aliens to the grid:
-addAliens()
-
-//* laserFly functioon runs from start but is only visible when shootLaser is called
-
-//* The below ensures a spaceship is generated at the start
-cells[spaceship].classList.remove('spaceship')
-cells[spaceship].classList.add('spaceship')
-
-
 //* Getting the aliens to move. This function starts the aliens moving
-aliensMovingRightInterval() 
 
 //* Event listener to listen for left and right keys
 document.addEventListener('keyup', (event) => {
@@ -57,6 +82,11 @@ document.body.onkeyup = function(e){
 
 //* -- Spaceship Functions --
 
+function addSpaceship() {
+  cells[spaceship].classList.remove('spaceship')
+  cells[spaceship].classList.add('spaceship')
+} 
+
 function moveSpaceshipLeft() {
   cells[spaceship].classList.remove('spaceship')
   spaceship -= 1
@@ -72,23 +102,26 @@ function moveSpaceshipRight() {
 //* -- Laser Functions --
 
 function shootLaser() {
-  let laserStart = spaceship
-  cells[laser].classList.remove('laser')
-  laser = laserStart -= width
-  cells[laser].classList.add('laser')
-  laserFly()
+  const laserStartPosition = spaceship - width
+  const laserId = Math.random() * Math.random() * 100
+  laserTimers[laserId] = { timerId: laserId , position: laserStartPosition }
+  const laserCurrentPosition = laserTimers[laserId].position
+  const laserTimer = laserTimers[laserId].timerId
+  cells[laserCurrentPosition].classList.add('laser')
+  laserFly(laserTimer, laserCurrentPosition)
 }
 
-function laserFly() {
+function laserFly(laserTimer, laser) {
   laserTimer = setInterval( () => {
     if (cells[laser].classList.contains('alien') ) {
       cells[laser].classList.add('explosion')
+      points += 100
+      pointsTotal.innerHTML = points
       currentAlienPositions =  currentAlienPositions.filter((alien) => {
         return !cells[alien].classList.contains('laser') 
       })
       cells[laser].classList.remove('laser')
       cells[laser].classList.remove('alien')
-      console.log('CURRENT ALIEN POSITIONS >', currentAlienPositions)
       setTimeout(() => {
         cells[laser].classList.remove('explosion')
       }, 200)
@@ -112,6 +145,7 @@ function addAliens () {
   aliensStart.forEach(alien => {
     cells[alien].classList.add('alien')
   })
+  aliensMovingRightInterval() 
 }
 
 //* This function moves the aliens right 1 cell. It does this by removing the alien from its current position, redefines the position, and then adds the alien to the new position.
@@ -180,6 +214,7 @@ function aliensMovingLeftInterval() {
     if (aliensReachGround()) {
       clearInterval(aliensMovingInterval)
       console.log('GAME OVER BOOOIIII')
+      alert('Game Over!')
     } else if (!alienOnLeftEdge) {
       moveAliensLeft()
     } else if (alienOnLeftEdge) {
@@ -195,7 +230,55 @@ function aliensReachGround() {
   return atTheGround
 }
 
-//* -- BUGS --
-  //* cant shoot more than 1 laser
-  //* explosion class not removing when multiple explosions
-  //* 
+//* ALIENS DROPPING BOMBS FUNCTIION: 
+
+function bombAppear() {
+  const randomNum = Math.floor(Math.random() * currentAlienPositions.length)
+  const alienToDropBomb = currentAlienPositions[randomNum]
+  const bombStartPosition = alienToDropBomb + width
+  if (cells[bombStartPosition].classList.contains('alien')) {
+    return bombAppear()
+  }
+  const bombId = Math.random() * Math.random() * 100
+  bombTimers[bombId] = { timerId: bombId , position: bombStartPosition }
+  const bombCurrentPosition = bombTimers[bombId].position
+  const bombTimer = bombTimers[bombId].timerId
+  cells[bombStartPosition].classList.add('bomb')
+  bombDrop(bombTimer, bombCurrentPosition)
+}
+
+function bombDrop(bombTimer, bombPosition) {
+  bombTimer = setInterval(() => {
+    if (cells[bombPosition].classList.contains('spaceship')) {
+      cells[bombPosition].classList.add('explosion')
+      cells[bombPosition].classList.remove('spaceship', 'bomb')
+      setTimeout(() => {
+        cells[bombPosition].classList.remove('explosion')
+      }, 200)
+      clearInterval(bombTimer)
+      //* Issue with below, doesnt always register:
+    } else if (cells[bombPosition].classList.contains('laser')) {
+      console.log('LASER HITS BOMB')
+      cells[bombPosition].classList.remove('laser')
+      cells[bombPosition].classList.remove('bomb')
+      cells[bombPosition].classList.add('explosion')
+    } else if (!(bombPosition + width >= width ** 2)) {
+      cells[bombPosition].classList.remove('bomb')
+      bombPosition = bombPosition += width
+      cells[bombPosition].classList.add('bomb')
+    } else {
+      cells[bombPosition].classList.remove('bomb')
+      clearInterval(bombTimer)
+    }
+  }, 500)
+}
+
+
+//* -- Other Functions --
+
+function loadDangerZone() { 
+  dangerZone.forEach(item => {
+    cells[item.index].innerText = item.letter.toUpperCase()
+    cells[item.index].classList.add(item.letter, 'dangerZone')
+  })
+}
